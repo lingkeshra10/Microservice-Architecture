fd2207794d609fccc7366d21ba160b5f4eca84c3
Lingkeshra Rajendram <99341331+lingkeshra10@users.noreply.github.com>
2026-06-02 22:24:54 +0800

    Upgrade Spring
    Upgrade Spring from 3.3.3 to 3.5.3

--- Microservice Source Code/LogsService/src/main/java/com/microservice/proto/LogsGrpc.java
+++ Microservice Source Code/LogsService/src/main/java/com/microservice/proto/LogsGrpc.java
@@ -5,14 +5,14 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.51.0)",
+    value = "by gRPC proto compiler (version 1.61.1)",
     comments = "Source: logs.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class LogsGrpc {
 
   private LogsGrpc() {}
 
-  public static final String SERVICE_NAME = "Logs";
+  public static final java.lang.String SERVICE_NAME = "Logs";
 
   // Static method descriptors that strictly reflect the proto.
   private static volatile io.grpc.MethodDescriptor<com.microservice.proto.addLogsParams,
@@ -123,45 +123,39 @@ public final class LogsGrpc {
 
   /**
    */
-  public static abstract class LogsImplBase implements io.grpc.BindableService {
+  public interface AsyncService {
 
     /**
      */
-    public void addLogs(com.microservice.proto.addLogsParams request,
+    default void addLogs(com.microservice.proto.addLogsParams request,
         io.grpc.stub.StreamObserver<com.microservice.proto.Empty> responseObserver) {
       io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getAddLogsMethod(), responseObserver);
     }
 
     /**
      */
-    public void retrieveUserLogs(com.microservice.proto.retrieveUserLogsParams request,
+    default void retrieveUserLogs(com.microservice.proto.retrieveUserLogsParams request,
         io.grpc.stub.StreamObserver<com.microservice.proto.APIResponse> responseObserver) {
       io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getRetrieveUserLogsMethod(), responseObserver);
-    }
-
-    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
-      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
-          .addMethod(
-            getAddLogsMethod(),
-            io.grpc.stub.ServerCalls.asyncUnaryCall(
-              new MethodHandlers<
-                com.microservice.proto.addLogsParams,
-                com.microservice.proto.Empty>(
-                  this, METHODID_ADD_LOGS)))
-          .addMethod(
-            getRetrieveUserLogsMethod(),
-            io.grpc.stub.ServerCalls.asyncUnaryCall(
-              new MethodHandlers<
-                com.microservice.proto.retrieveUserLogsParams,
-                com.microservice.proto.APIResponse>(
-                  this, METHODID_RETRIEVE_USER_LOGS)))
-          .build();
     }
   }
 
   /**
+   * Base class for the server implementation of the service Logs.
    */
-  public static final class LogsStub extends io.grpc.stub.AbstractAsyncStub<LogsStub> {
+  public static abstract class LogsImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return LogsGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service Logs.
+   */
+  public static final class LogsStub
+      extends io.grpc.stub.AbstractAsyncStub<LogsStub> {
     private LogsStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -191,8 +185,10 @@ public final class LogsGrpc {
   }
 
   /**
+   * A stub to allow clients to do synchronous rpc calls to service Logs.
    */
-  public static final class LogsBlockingStub extends io.grpc.stub.AbstractBlockingStub<LogsBlockingStub> {
+  public static final class LogsBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<LogsBlockingStub> {
     private LogsBlockingStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -220,8 +216,10 @@ public final class LogsGrpc {
   }
 
   /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service Logs.
    */
-  public static final class LogsFutureStub extends io.grpc.stub.AbstractFutureStub<LogsFutureStub> {
+  public static final class LogsFutureStub
+      extends io.grpc.stub.AbstractFutureStub<LogsFutureStub> {
     private LogsFutureStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -258,10 +256,10 @@ public final class LogsGrpc {
       io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
       io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
       io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
-    private final LogsImplBase serviceImpl;
+    private final AsyncService serviceImpl;
     private final int methodId;
 
-    MethodHandlers(LogsImplBase serviceImpl, int methodId) {
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
       this.serviceImpl = serviceImpl;
       this.methodId = methodId;
     }
@@ -294,6 +292,25 @@ public final class LogsGrpc {
     }
   }
 
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getAddLogsMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              com.microservice.proto.addLogsParams,
+              com.microservice.proto.Empty>(
+                service, METHODID_ADD_LOGS)))
+        .addMethod(
+          getRetrieveUserLogsMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              com.microservice.proto.retrieveUserLogsParams,
+              com.microservice.proto.APIResponse>(
+                service, METHODID_RETRIEVE_USER_LOGS)))
+        .build();
+  }
+
   private static abstract class LogsBaseDescriptorSupplier
       implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
     LogsBaseDescriptorSupplier() {}
@@ -317,9 +334,9 @@ public final class LogsGrpc {
   private static final class LogsMethodDescriptorSupplier
       extends LogsBaseDescriptorSupplier
       implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
-    private final String methodName;
+    private final java.lang.String methodName;
 
-    LogsMethodDescriptorSupplier(String methodName) {
+    LogsMethodDescriptorSupplier(java.lang.String methodName) {
       this.methodName = methodName;
     }
 
--- Microservice Source Code/PasswordService/src/main/java/com/microservice/proto/LogsGrpc.java
+++ Microservice Source Code/PasswordService/src/main/java/com/microservice/proto/LogsGrpc.java
@@ -5,14 +5,14 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.51.0)",
+    value = "by gRPC proto compiler (version 1.61.1)",
     comments = "Source: logs.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class LogsGrpc {
 
   private LogsGrpc() {}
 
-  public static final String SERVICE_NAME = "Logs";
+  public static final java.lang.String SERVICE_NAME = "Logs";
 
   // Static method descriptors that strictly reflect the proto.
   private static volatile io.grpc.MethodDescriptor<com.microservice.proto.addLogsParams,
@@ -92,31 +92,32 @@ public final class LogsGrpc {
 
   /**
    */
-  public static abstract class LogsImplBase implements io.grpc.BindableService {
+  public interface AsyncService {
 
     /**
      */
-    public void addLogs(com.microservice.proto.addLogsParams request,
+    default void addLogs(com.microservice.proto.addLogsParams request,
         io.grpc.stub.StreamObserver<com.microservice.proto.Empty> responseObserver) {
       io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall(getAddLogsMethod(), responseObserver);
-    }
-
-    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
-      return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
-          .addMethod(
-            getAddLogsMethod(),
-            io.grpc.stub.ServerCalls.asyncUnaryCall(
-              new MethodHandlers<
-                com.microservice.proto.addLogsParams,
-                com.microservice.proto.Empty>(
-                  this, METHODID_ADD_LOGS)))
-          .build();
     }
   }
 
   /**
+   * Base class for the server implementation of the service Logs.
    */
-  public static final class LogsStub extends io.grpc.stub.AbstractAsyncStub<LogsStub> {
+  public static abstract class LogsImplBase
+      implements io.grpc.BindableService, AsyncService {
+
+    @java.lang.Override public final io.grpc.ServerServiceDefinition bindService() {
+      return LogsGrpc.bindService(this);
+    }
+  }
+
+  /**
+   * A stub to allow clients to do asynchronous rpc calls to service Logs.
+   */
+  public static final class LogsStub
+      extends io.grpc.stub.AbstractAsyncStub<LogsStub> {
     private LogsStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -138,8 +139,10 @@ public final class LogsGrpc {
   }
 
   /**
+   * A stub to allow clients to do synchronous rpc calls to service Logs.
    */
-  public static final class LogsBlockingStub extends io.grpc.stub.AbstractBlockingStub<LogsBlockingStub> {
+  public static final class LogsBlockingStub
+      extends io.grpc.stub.AbstractBlockingStub<LogsBlockingStub> {
     private LogsBlockingStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -160,8 +163,10 @@ public final class LogsGrpc {
   }
 
   /**
+   * A stub to allow clients to do ListenableFuture-style rpc calls to service Logs.
    */
-  public static final class LogsFutureStub extends io.grpc.stub.AbstractFutureStub<LogsFutureStub> {
+  public static final class LogsFutureStub
+      extends io.grpc.stub.AbstractFutureStub<LogsFutureStub> {
     private LogsFutureStub(
         io.grpc.Channel channel, io.grpc.CallOptions callOptions) {
       super(channel, callOptions);
@@ -189,10 +194,10 @@ public final class LogsGrpc {
       io.grpc.stub.ServerCalls.ServerStreamingMethod<Req, Resp>,
       io.grpc.stub.ServerCalls.ClientStreamingMethod<Req, Resp>,
       io.grpc.stub.ServerCalls.BidiStreamingMethod<Req, Resp> {
-    private final LogsImplBase serviceImpl;
+    private final AsyncService serviceImpl;
     private final int methodId;
 
-    MethodHandlers(LogsImplBase serviceImpl, int methodId) {
+    MethodHandlers(AsyncService serviceImpl, int methodId) {
       this.serviceImpl = serviceImpl;
       this.methodId = methodId;
     }
@@ -221,6 +226,18 @@ public final class LogsGrpc {
     }
   }
 
+  public static final io.grpc.ServerServiceDefinition bindService(AsyncService service) {
+    return io.grpc.ServerServiceDefinition.builder(getServiceDescriptor())
+        .addMethod(
+          getAddLogsMethod(),
+          io.grpc.stub.ServerCalls.asyncUnaryCall(
+            new MethodHandlers<
+              com.microservice.proto.addLogsParams,
+              com.microservice.proto.Empty>(
+                service, METHODID_ADD_LOGS)))
+        .build();
+  }
+
   private static abstract class LogsBaseDescriptorSupplier
       implements io.grpc.protobuf.ProtoFileDescriptorSupplier, io.grpc.protobuf.ProtoServiceDescriptorSupplier {
     LogsBaseDescriptorSupplier() {}
@@ -244,9 +261,9 @@ public final class LogsGrpc {
   private static final class LogsMethodDescriptorSupplier
       extends LogsBaseDescriptorSupplier
       implements io.grpc.protobuf.ProtoMethodDescriptorSupplier {
-    private final String methodName;
+    private final java.lang.String methodName;
 
-    LogsMethodDescriptorSupplier(String methodName) {
+    LogsMethodDescriptorSupplier(java.lang.String methodName) {
       this.methodName = methodName;
     }
 
